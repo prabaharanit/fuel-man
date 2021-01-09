997d1089da1fbc861febf47304f8ab91e4df4b31
prabaharanit@gmail.com <prabaharanit@gmail.com>
2021-01-09 15:09:20 +0530

    added tests

--- src/test/java/com/mayavan/fuelman/service/CreditBookServiceImplTest.java
+++ src/test/java/com/mayavan/fuelman/service/CreditBookServiceImplTest.java
@@ -2,13 +2,17 @@ package com.mayavan.fuelman.service;
 
 import java.text.ParseException;
 
+import javax.transaction.Transactional;
+
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mayavan.fuelman.repo.model.CreditBookMO;
 import com.mayavan.fuelman.repo.model.FuelPriceMO;
+import com.mayavan.fuelman.repo.model.FuelTypeMO;
 import com.mayavan.fuelman.repo.model.VehicleMO;
 import com.mayavan.fuelman.util.DateAndTimeUtility;
 import com.mayavan.fuelman.util.DateAndTimeUtility.DATEFORMAT;
@@ -20,7 +24,8 @@ class CreditBookServiceImplTest {
 	@Autowired
 	private CreditBookService creditBookService;
 
-	@Test
+	@Ignore
+	@Transactional
 	public void testGetFuelPriceForDttmOfSale() {
 		try {
 			CreditBookMO creditBookMO = getCreditBookMO();
@@ -32,6 +37,16 @@ class CreditBookServiceImplTest {
 	}
 	
 	
+	@Test
+	public void testAllCreditBookEntries() {
+		try {
+			creditBookService.getAllCreditBook();
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail("get fuel price for dttm for sale");
+		}
+	}
+	
 	private CreditBookMO getCreditBookMO() {
 		CreditBookMO creditBookMO = new CreditBookMO();
 		creditBookMO.setAmount_of_sale(23243.78);
@@ -40,10 +55,21 @@ class CreditBookServiceImplTest {
 		vehicleMO.setId(1);
 		creditBookMO.setVehicleMO(vehicleMO);
 		FuelPriceMO fuelPriceMO = new FuelPriceMO();
-		fuelPriceMO.setId(1);
+		FuelTypeMO fuelType = new FuelTypeMO();
+		fuelType.setId(2);
+		fuelPriceMO.setFuel_type(fuelType);
+		fuelPriceMO.setPrice(78);
 		creditBookMO.setFuelPriceMO(fuelPriceMO);
+		creditBookMO.setComments("test data");
 		try {
-			creditBookMO.setDttm_of_sale(DateAndTimeUtility.convertStringToTimestamp("30 12 2020 13:31:16", DATEFORMAT.CLIENT_DATE_TIME_FORMAT_DTTM_OF_SALE));
+			creditBookMO.setDttm_of_sale(DateAndTimeUtility.changeDateFormat("30 12 2020 13:31:16",
+					DATEFORMAT.DB_DATE_TIME_FORMAT, DATEFORMAT.CLIENT_DATE_TIME_FORMAT_DTTM_OF_SALE));
+			try {
+				creditBookService.createCreditBook(creditBookMO);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
