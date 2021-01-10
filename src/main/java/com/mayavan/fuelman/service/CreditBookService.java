de1c4f80bceb82532fd98c3acd5b15c10920c3ed
prabaharanit@gmail.com <prabaharanit@gmail.com>
2021-01-10 09:11:48 +0530

    added create credit transaction

--- src/main/java/com/mayavan/fuelman/service/CreditBookService.java
+++ src/main/java/com/mayavan/fuelman/service/CreditBookService.java
@@ -6,13 +6,16 @@ import org.springframework.stereotype.Service;
 
 import com.mayavan.fuelman.exception.ResourceNotFoundException;
 import com.mayavan.fuelman.repo.model.CreditBookMO;
+import com.mayavan.fuelman.repo.model.CreditTransactionMO;
 
 @Service
 public interface CreditBookService {
 	
 	public CreditBookMO createCreditBook(CreditBookMO creditBookMO)throws Exception;
 	
-    public List<CreditBookMO> getAllCreditBook() throws ResourceNotFoundException;
+	public CreditTransactionMO createCreditTransaction(CreditTransactionMO creditTransactionMO)throws Exception;
+	
+	public List<CreditBookMO> getAllCreditBook() throws ResourceNotFoundException;
     
     public CreditBookMO getCreditBookForId(Integer Id)throws ResourceNotFoundException;
     
