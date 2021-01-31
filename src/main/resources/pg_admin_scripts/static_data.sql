260870aa06208a1cc4b2293557939e02d5a010c3
prabaharanit <prabaharanit@gmail.com>
2021-01-31 11:52:46 +0530

    initial version for static data sql file

---
+++ src/main/resources/pg_admin_scripts/static_data.sql
@@ -0,0 +1,15 @@
+
+--
+-- package_type static data
+--
+
+insert into package_type (type, unit) values ('Can', 'liters');
+insert into package_type (type, unit) values ('Box', 'pieces');
+
+
+--
+-- product_type static data
+--
+
+insert into product_type (type) values ('fuel');
+insert into product_type (type) values ('lubricants');
